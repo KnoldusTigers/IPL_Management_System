ebc31986e5c97f4250c6a6a2bd45b23bb130bf53
Akshit <akshit.kumar@knoldus.com>
2021-12-14 18:17:02 +0530

    updated by akshit 14dec fix bugs

--- src/main/java/com/controller/MatchController.java
+++ src/main/java/com/controller/MatchController.java
@@ -6,10 +6,10 @@ import com.model.MatchModel;
 import com.model.StateModel;
 import com.model.TeamModel;
 import com.service.MatchService;
+import com.service.ResultService;
 import com.service.StateService;
 import com.service.TeamService;
-import com.service.ResultService;
-import org.springframework.beans.factory.annotation.Autowired;
+ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -20,10 +20,6 @@ import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
 import java.util.List;
-
-/**
- * The type Match controller.
- */
 @Controller
 public class MatchController {
 
@@ -35,12 +31,6 @@ public class MatchController {
     private TeamService teamService;
     @Autowired
     private StateService stateService;
-    /**
-     * View match string.
-     *
-     * @param model the model
-     * @return the string
-     */
     @GetMapping(value = "/viewMatch")
     public String viewMatch(final Model model) {
         List<MatchModel> matchModelsList = matchService.getAllMatches();
@@ -50,13 +40,7 @@ public class MatchController {
         return "viewMatch";
     }
 
-    /**
-     * Add match string.
-     *
-     * @param model the model
-     * @return the string
-     */
-//return match scheduling page
+    //return match scheduling page
     @GetMapping(value = "/matchSchedule")
     public String addMatch(final Model model) {
         model.addAttribute("match", new MatchModel());
@@ -67,64 +51,36 @@ public class MatchController {
         return "matchSchedule";
     }
 
-    /**
-     * Save match string.
-     *
-     * @param match  the match
-     * @param result the result
-     * @return the string
-     */
-//Save a scheduled match and return veiw match page
+    //Save a scheduled match and return veiw match page
     @RequestMapping(value = "/SaveMatch", method = RequestMethod.POST)
-    public String saveMatch(@Valid @ModelAttribute("match") final MatchModel match, final BindingResult result) {
-
+    public String saveMatch(@Valid @ModelAttribute("match") MatchModel match, final BindingResult result) {
+//        if(matchService.venueExists(match.getVenue(),result)) {
             if (matchService.DateIsExist(match.getScheduledate())) {
-//                if (matchService.venueExists(match.getVenue(), result)) {
-
-                    result.addError(new FieldError("match", "scheduledate", "date or venue already exists"));
-                }
-//            }
+                result.addError(new FieldError("match", "scheduledate", "date or venue already exists"));
+            }
 
         if (result.hasErrors()) {
-            return "matchSchedule"; }
+            return "matchSchedule";}
         else {
+            //match=reservice.getResult(match);
+            // System.out.println(match.getResult()+"==========================================================");
 
             matchService.save(match);
             return "redirect:viewMatch";
         }
-//        if(matchService.cehckVenue(match.getScheduledate(),match.getVenue())){
-//            System.out.println("=================--------------  check vueneu is exit");
-//
-//        }
-//
-//        return "";
     }
 
-    /**
-     * Updatescore string.
-     *
-     * @param match  the match
-     * @param result the result
-     * @return the string
-     */
-//Save score in daatabase team1 and team2
     @RequestMapping(value = "/SaveScore", method = RequestMethod.POST)
     public String updatescore(@Valid @ModelAttribute("match") MatchModel match, final BindingResult result) {
-        if (result.hasErrors()) {
+        if(result.hasErrors()){
 
         }
-        match = reservice.getResult(match);
+        match=reservice.getResult(match);
         matchService.save(match);
         return "redirect:EditListScore";
     }
 
-    /**
-     * View score string.
-     *
-     * @param model the model
-     * @return the string
-     */
-//open update score page where we add score
+    //
     @GetMapping(value = "/UpdateScore")
     public String viewScore(final Model model) {
         List<MatchModel> matchModelsList = matchService.getAllMatches();
@@ -132,13 +88,7 @@ public class MatchController {
         return "UpdateScore";
     }
 
-    /**
-     * Edit score string.
-     *
-     * @param model the model
-     * @return the string
-     */
-// edit score List page
+    // edit score List
     @GetMapping(value = "/EditListScore")
     public String editScore(final Model model) {
         List<MatchModel> matchModelsList = matchService.getAllMatches();
@@ -146,14 +96,6 @@ public class MatchController {
 
         return "EditListScore";
     }
-
-    /**
-     * Show edit score model and view.
-     *
-     * @param id the id
-     * @return the model and view
-     */
-// editscore by matchid
     @RequestMapping("/editScore/{matchid}")
     public ModelAndView showEditScore(@PathVariable(name = "matchid") final String id) {
         ModelAndView modelAndView = new ModelAndView("UpdateScore");
@@ -166,13 +108,7 @@ public class MatchController {
         return modelAndView;
     }
 
-    /**
-     * Show edit pllayer model and view.
-     *
-     * @param id the id
-     * @return the model and view
-     */
-//edit a match
+    //edit a match
     @RequestMapping("/editmatch/{matchid}")
     public ModelAndView showEditPllayer(@PathVariable(name = "matchid") final String id) {
         ModelAndView modelAndView = new ModelAndView("updateMatch");
@@ -187,40 +123,20 @@ public class MatchController {
         return modelAndView;
     }
 
-    /**
-     * Deletestudent string.
-     *
-     * @param id the id
-     * @return the string
-     */
-//delete a match
+    //delete a match
     @RequestMapping("/deletematch/{matchid}")
     public String deletestudent(@PathVariable(name = "matchid") final String id) {
         matchService.delete(Integer.parseInt(id));
         return "redirect:/editMatch";
     }
-
-    /**
-     * View match result string.
-     *
-     * @param model the model
-     * @return the string
-     */
-//show result
+    //show result
     @GetMapping(value = "/result")
     public String viewMatchResult(final Model model) {
         List<MatchModel> matchModelsList = matchService.getAllMatches();
         model.addAttribute("matchModelsList", matchModelsList);
         return "result";
     }
-
-    /**
-     * Edit match string.
-     *
-     * @param model the model
-     * @return the string
-     */
-//edit a match page
+    //edit a match page
     @GetMapping(value = "/editMatch")
     public String editMatch(final Model model) {
         List<MatchModel> matchModelsList = matchService.getAllMatches();
